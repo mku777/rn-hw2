32ff96d690f7f7bd70de8b456e426c5be626edb3
mku777 <mkm.kuz777@gmail.com>
2023-03-15 15:15:44 +0200

    screens

--- App.js
+++ App.js
@@ -1,20 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar  } from 'expo-status-bar';
+import LoginScreen from "./Screens/LoginScreen/LoginScreen";
+import RegistrationScreen from "./Screens/RegistrationScreen/RegistrationScreen";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>homework1</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      {/* <RegistrationScreen /> */}
+      <LoginScreen />
+       <StatusBar style="auto" />  
+    </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
